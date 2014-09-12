025c5318626e0e5ae05fa3d54c97ebab79ad2acb
RedOrion <redorion0@gmail.com>
2014-09-12 09:03:55 -0400

    Cleaned up formatting of code

--- RandomDiceConsole/Module1.vb
+++ RandomDiceConsole/Module1.vb
@@ -2,19 +2,23 @@
 
 
     Sub Main()
-        Randomize()
+        RandomizeSixSidedDie()
+        RandomizeZeroAndOneHundred()
+        Console.ReadKey()
     End Sub
 
-    Sub Randomize()
-        Dim lowerbound As Integer = 0
-        Dim upperbound As Integer = 100
+    Sub RandomizeSixSidedDie()
         Dim sixsideddie As Integer = CInt(Int((6 * Rnd()) + 1))
-        Dim zeroandonehundred As Integer = CInt(Math.Floor((upperbound - lowerbound + 1) * Rnd())) + lowerbound
         Console.WriteLine("Six Sided Die")
         Console.WriteLine(sixsideddie)
+    End Sub
+    
+    Sub RandomizeZeroAndOneHundred()
+        Dim lowerbound As Integer = 0
+        Dim upperbound As Integer = 100
+        Dim zeroandonehundred As Integer = CInt(Math.Floor((upperbound - lowerbound + 1) * Rnd())) + lowerbound
         Console.WriteLine("Value Between 0 and 100")
         Console.WriteLine(zeroandonehundred)
-        Console.ReadKey()
     End Sub
 
 End Module
